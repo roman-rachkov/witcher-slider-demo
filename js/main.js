de504593a9206dedd28ad4f85fcaeee78e3a7cd3
Rachkov Roman <pakycb84@gmail.com>
2023-01-17 14:09:10 +1000

    init

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new Swiper('.swiper.slider', {
+        speed: 2400,
+        // loop: true,
+        // freeMode: true,
+        mousewheel: {
+            enabled: true,
+            speed: 2.4  
+        },
+        spaceBetween: 30,
+        effect: 'slides',
+        // autoplay: {
+        //     delay: 5000
+        // },
+        parallax: true,
+        keyboard:{
+            enabled: true
+        }
+    })
+});
